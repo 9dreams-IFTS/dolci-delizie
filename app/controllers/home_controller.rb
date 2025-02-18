d8b16642179fde46e20dd55663649d1914e56871
PokemonHeartGoldEnjoyer <e.simakou@donboscosandona.it>
2025-02-18 13:16:30 +0100

    Iniziato Colazione

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
     def index  
     end
+    def contatti
+    end
+    def colazione
+    end
+    def pranzo
+    end
+    def merenda
+    end
+    def cena
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get '/home', to: 'home#index', as: 'home'
+  get '/contatti', to: 'home#contatti', as:'contatti'
+  get '/colazione', to: 'home#colazione', as:'colazione'
+  get '/pranzo', to: 'home#pranzo', as:'pranzo'
+  get '/merenda', to: 'home#merenda', as:'merenda'
+  get '/cena', to: 'home#cena', as:'cena'
   root 'home#index'
 end
